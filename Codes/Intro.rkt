71eb7a5bfe008d2f00c3bc6a1d716c83a91eee52
Abraham Xiao <q289196573@gmail.com>
2014-09-24 03:40:14 +0400

    Update notes and sample code. Reading the textbook is one thing. Learning how to programming as an expert is another. Take a good sleep for about 5 hours. Get up and enjoy meals, courses and programming. Life is awesome in the United Arab Emirates.
    Write your blog, and do your fucking research.

--- Codes/Intro.rkt
+++ Codes/Intro.rkt
@@ -19,6 +19,8 @@
 
 (+ (* 5 5) (+ (* 3 (/ 12 4)) 4))
 
+
+; Somet
 (+ (1 (2)))
 (+ (1) (2))
 
@@ -27,9 +29,48 @@
 (+ 1 2 3 4 5 6 7 8 9 0)
 (* 1 2 3 4 5 6 7 8 9 0)
 
+; Arithmetic and Arithmetic
 (string-append "hello" "world")
+(string-append "hello " "world")
+(string-append "hell" "o" "world")
+(string-append "hello" " " "world")
+
+(+ (string-length "hello world") 20)
+
+(number->string 42)
+(string->number 42) ; This will not work!
+(string->number "42") ; This will work
+
+(string->number "hello world")
+
+(and true true)
+(and true false)
+(or true false)
+(or false false)
+(not false)
+
+(> 10 9)
+(< -1 0)
+(= 42 9)
+
+(>= 10 10)
+(<= -1 0)
+(string? "design" "tinker") ; This will NOT work
+(string=? "design" "tinker")
 
 
 
 ; Input and Output
 (define (y x) (* x x))
+
+
+; Emacs Scribble mode, which is not fully-fledged.
+(require (planet neil/scribble-emacs:1:3/scribble-emacs-dotemacs))
+
+(require (planet neil/scribble-emacs:1:3))
+
+(require (planet neil/scribble-emacs:1:3/<<file>>))
+
+#|
+Life is awesome
+|#
