f820585b214fb34fd89179ec169553af408b6917
Abraham Xiao <q289196573@gmail.com>
2018-04-25 22:29:03 +0800

    Add some codes to the project.

---
+++ Codes/SATAOP/book1-3.scm
@@ -0,0 +1,26 @@
+7
+
+ten
+
+(define ten 10)
+
+; (define var expr)
+; (quote symbol)
+
+(quote ten)
+
+(quote abc3)
+
+(quote 12)
+
+'abc3
+
+'12
+
+(define Robert 'Bob)
+
+(+ 3 4 5 6 7 8 9 10 11 12)
+
+(* 3 (- 12 5))
+
+(+ 2 (/ 30 15))
---
+++ Codes/SATAOP/exe1-1.scm
@@ -0,0 +1,13 @@
+; These are answers to exercise 1.1
+
+15
+
+-200
+
+12345678901234
+
+(quote alphabet-soup)
+
+'alphabet-soup
+
+''alphabet-soup
