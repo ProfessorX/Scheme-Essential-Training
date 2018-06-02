e000e80b8f3814730cd4ae98c7b831f504546c0a
Abraham Xiao <q289196573@gmail.com>
2018-06-02 16:14:22 +0800

    Add codes and answers to exe1-3.

--- Codes/SATAOP/exe1-1.scm
+++ Codes/SATAOP/exe1-1.scm
@@ -1,5 +1,6 @@
-; These are answers to exercise 1.1
+; These are answers to exercises of ch1-3
 
+; exercise 1.1
 15
 
 -200
@@ -11,3 +12,23 @@
 'alphabet-soup
 
 ''alphabet-soup
+
+;;; exercise 1.2
+
+(define big-number 10500900)
+(define small-number 0.00000025)
+(define cheshire 'cat)
+(define number1 big-number)
+(define number2 'big-number)
+
+big-number
+small-number
+'big-number
+cheshire
+'cheshire
+number1
+number2
+'number1
+
+;;; exercise 1.3
+
